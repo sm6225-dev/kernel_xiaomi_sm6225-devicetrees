63d5b57afc805ffbd13eac51f62bbb1291cdf6bb
Bruno Martins <bgcngm@gmail.com>
2025-09-14 00:27:50 +0530

    treewide: Opt-out for irrelevant DTB[O]s based on config
    Change-Id: Ic2bcdb5533f21005708868e4168391563c9961c7
    
    [muralivijay: adapt changes for spes]
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- qcom/camera/config/bengal.mk
+++ qcom/camera/config/bengal.mk
@@ -1,15 +1,19 @@
 dtbo-$(CONFIG_ARCH_KHAJE) := khaje-camera.dtbo
+
+ifeq ($(CONFIG_TARGET_PROJECT_K7T),y)
+dtbo-y += spes-khaje-camera-sensor-idp-nopmi.dtbo \
+          spes-sm5602-khaje-camera-sensor-idp-nopmi.dtbo \
+          spesn-khaje-camera-sensor-idp-nopmi.dtbo
+else
 dtbo-$(CONFIG_ARCH_KHAJE) += khajeg-camera-idp.dtbo \
 				khajeg-camera-qrd.dtbo \
 				khajeg-camera-qrd-hvdcp3p5.dtbo \
 				khaje-camera-sensor-idp.dtbo \
 				khaje-camera-sensor-qrd.dtbo \
 				khaje-camera-sensor-idp-nopmi.dtbo \
-                                spes-khaje-camera-sensor-idp-nopmi.dtbo \
-                                spes-sm5602-khaje-camera-sensor-idp-nopmi.dtbo \
-                                spesn-khaje-camera-sensor-idp-nopmi.dtbo \
 				khaje-camera-sensor-qrd-nopmi.dtbo \
 				khaje-camera-sensor-qrd-hvdcp3p5.dtbo \
 				khaje-camera-sensor-qrd-nowcd9375.dtbo \
 				khaje-camera-sensor-idps-display-90hz.dtbo \
-				khaje-camera-sensor-idp-usbc.dtbo \
+				khaje-camera-sensor-idp-usbc.dtbo
+endif
